301e97604d3bfadada4ebc06c83ee261941ed0e1
thanhdx <thanhdx@coccoc.vn>
2018-11-29 14:41:38 +0700

    Test query name

--- web-services/server.py
+++ web-services/server.py
@@ -1,37 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
+import json
+from datetime import datetime
+from elasticsearch import Elasticsearch
+
+es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
-    return render_template('index.html', results=[{
-        "title": "hello",
-        "content": "Lorem ipsum dolor sit amet..."
-    },
-    {
-        "title": "hey",
-        "content": "Lorem ipsum dolor sit amet..."
-    },
-    {
-        "title": "hi there",
-        "content": "Lorem ipsum dolor sit amet..."
-    },
-    {
-        "title": "hi",
-        "content": "Lorem ipsum dolor sit amet..."
-    },
-    {
-        "title": "good evening",
-        "content": "Lorem ipsum dolor sit amet..."
-    },
-    {
-        "title": "good afternoon",
-        "content": "Lorem ipsum dolor sit amet..."
-    },
-    {
-        "title": "good morning",
-        "content": "Lorem ipsum dolor sit amet..."
-    }
-    ])
+    return render_template('index.html', results=None)
+
+
+@app.route('/search')
+def search():
+    query = request.args.get('query')
+    results = es.search(index="swapi", body={"query": {"prefix" : { "name" : query }}})
+    print(results)
+    return render_template('index.html', results={
+        "detail": {
+            "query": query,
+            "total": results['hits']['total'],
+            "took": results['took']
+        },
+        "hits": results['hits']['hits']
+    })
+
 
 if __name__ == '__main__':
     app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 10
